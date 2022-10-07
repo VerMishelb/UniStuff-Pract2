6772721576f6bb2673efbd9ce001dc45bf72ee63
VerMishelb <57833258+VerMishelb@users.noreply.github.com>
2022-10-07 10:23:13 +0300

    Base for a Point class

---
+++ src/ru/mish/task3/Point.java
@@ -0,0 +1,25 @@
+package ru.mish.task3;
+
+class Point {
+    //Public
+
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    //Private
+    double x, y;
+}
